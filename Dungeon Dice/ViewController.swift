70d67e046efdc3857b92815eabd944f6c2bf3c15
Mr. Michael Kelley <mx@kell4.com>
2019-01-28 17:50:06 -0500

    Initial Commit

---
+++ Dungeon Dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dungeon Dice
+//
+//  Created by Michael X Kelley on 1/28/19.
+//  Copyright © 2019 Michael X Kelley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
